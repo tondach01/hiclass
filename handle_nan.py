3b4b6ed9e53ef5b9811a9c9c22f26a0c4c2e1c47
tondach01 <ondra.metelka@seznam.cz>
2024-05-22 09:22:40 +0200

    add removing NaN rows

--- handle_nan.py
+++ handle_nan.py
@@ -2,11 +2,16 @@ import pandas as pd
 from sklearn.impute import SimpleImputer, KNNImputer
 
 
-def impute_mean(x):
+def impute_mean(x: pd.DataFrame):
     imp = SimpleImputer(strategy="mean").fit(x)
     return pd.DataFrame(imp.transform(x), columns=imp.feature_names_in_)
 
 
-def impute_knn(x, k=5):
-    imp = KNNImputer(n_neighbors=k)
-    return imp.fit_transform(x)
+def impute_knn(x: pd.DataFrame, k=5):
+    imp = KNNImputer(n_neighbors=k).fit(x)
+    return pd.DataFrame(imp.transform(x), columns=imp.feature_names_in_)
+
+
+def remove_nan(x: pd.DataFrame, y: pd.DataFrame):
+    nan_rows = x[x.isnull().T.any()]
+    return x.drop(labels=nan_rows), y.drop(labels=nan_rows)
